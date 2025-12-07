349d2b3cf8fa304deb35a6f4129612e60de568f3
AbeerPanwar <panwarabeer6@gmail.com>
2025-12-07 17:52:42 +0530

    fixed launching issue and changed the background

--- lib/features/Auth/view/pages/auth_page.dart
+++ lib/features/Auth/view/pages/auth_page.dart
@@ -34,99 +34,111 @@ class _AuthScreenState extends State<AuthScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(24),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: Text(
-                      'Nothing Music',
-                      style: Theme.of(context).textTheme.headlineLarge
-                          ?.copyWith(
-                            color: Pallete.gradient1,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Zain',
-                          ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.grey.shade800, Colors.black12 , Colors.black54],
+            begin: Alignment.topCenter,
+            end: AlignmentGeometry.bottomCenter,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Text(
+                        'Nothing Music',
+                        style: Theme.of(context).textTheme.headlineLarge
+                            ?.copyWith(
+                              color: Pallete.gradient1,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Zain',
+                            ),
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 8),
-                  Center(
-                    child: Text(
-                      'Enjoy music — Anytime, Anywhere',
-                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                        color: Pallete.whiteColor,
+                    const SizedBox(height: 8),
+                    Center(
+                      child: Text(
+                        'Enjoy music — Anytime, Anywhere',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          color: Pallete.whiteColor,
+                          fontFamily: 'Zain',
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 32),
+                    Text(
+                      _tabController.index == 0 ? 'Welcome' : 'Sign Up',
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: Pallete.geryGradiant1,
                         fontFamily: 'Zain',
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 32),
-                  Text(
-                    _tabController.index == 0 ? 'Welcome' : 'Sign Up',
-                    style: TextStyle(
-                      fontSize: 26,
-                      fontWeight: FontWeight.bold,
-                      color: Pallete.geryGradiant1,
-                      fontFamily: 'Zain',
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  Text(
-                    _tabController.index == 0
-                        ? 'To get started, please sign in using your username and password.'
-                        : 'To get started, please sign up inputing below fields information.',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 14,
-                      color: Pallete.geryGradiant2,
-                      fontFamily: 'Zain',
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.primaryContainer,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Container(
-                      decoration: BoxDecoration(
+                    const SizedBox(height: 5),
+                    Text(
+                      _tabController.index == 0
+                          ? 'To get started, please sign in using your username and password.'
+                          : 'To get started, please sign up inputing below fields information.',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
                         color: Pallete.geryGradiant2,
+                        fontFamily: 'Zain',
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primaryContainer,
                         borderRadius: BorderRadius.circular(20),
                       ),
-                      child: TabBar(
-                        overlayColor: WidgetStatePropertyAll(
-                          Pallete.backgroundColor,
-                        ),
-                        controller: _tabController,
-                        indicator: BoxDecoration(
-                          color: Pallete.geryGradiant3,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Pallete.geryGradiant2,
                           borderRadius: BorderRadius.circular(20),
                         ),
-                        dividerColor: Pallete.backgroundColor,
-                        indicatorSize: TabBarIndicatorSize.tab,
-                        labelColor: Pallete.gradient1,
-                        labelStyle: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'Zain',),
-                        unselectedLabelColor: Pallete.backgroundColor,
-                        tabs: const [
-                          Tab(text: 'Sign In'),
-                          Tab(text: 'Sign Up'),
-                        ],
+                        child: TabBar(
+                          overlayColor: WidgetStatePropertyAll(
+                            Pallete.backgroundColor,
+                          ),
+                          controller: _tabController,
+                          indicator: BoxDecoration(
+                            color: Pallete.geryGradiant3,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          dividerColor: Pallete.backgroundColor,
+                          indicatorSize: TabBarIndicatorSize.tab,
+                          labelColor: Pallete.gradient1,
+                          labelStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Zain',
+                          ),
+                          unselectedLabelColor: Pallete.backgroundColor,
+                          tabs: const [
+                            Tab(text: 'Sign In'),
+                            Tab(text: 'Sign Up'),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: TabBarView(
-                controller: _tabController,
-                children: const [SignInForm(), SignUpForm()],
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: const [SignInForm(), SignUpForm()],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Music Player',
+      title: 'Nothing Music',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.darkThemeMode,
       home: const AuthScreen(),
